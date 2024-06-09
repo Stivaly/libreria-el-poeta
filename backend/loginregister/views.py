3a6a9d3b02fe45a8e1531e74f617f0b237d60c65
Stivaly Gomez <133545882+Stivaly@users.noreply.github.com>
2024-06-09 14:37:07 -0400

    Actualización 1 views.py
    Se remueve la función de signup y se agrega las funciones de login, profile y signout

--- backend/loginregister/views.py
+++ backend/loginregister/views.py
@@ -1,15 +1,33 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
-def signup(request):
+def login_view(request):
     if request.user.is_authenticated:
-        return redirect('/')
+        return render(request, 'home.html')
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/profile')
+        else:
+            msg = 'Invalid credentials'
+            form = AuthenticationForm(request.POST)
+            return render(request, 'login.html', {'form': form, 'msg': msg})
     else:
-        form = UserCreationForm()
-        return render(request,'signup.html', {'form': form})
+        form = AuthenticationForm()
+        return render(request,'login.html', {'form': form})
+    
+def profile(request):
+    return render(request, 'profile.html')
+
+def signout(request):
+    logout(request)
+    return redirect('/home')
